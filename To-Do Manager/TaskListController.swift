1f0c60156ca35f71b6c14bdb78c4c09b15d93471
Yuriy <ura.yakovlev@yandex.ru>
2022-10-25 00:38:21 +0500

    Добавлена функциональность вывода списка дел на экран

--- To-Do Manager/TaskListController.swift
+++ To-Do Manager/TaskListController.swift
@@ -1,9 +1,13 @@
 import UIKit
 
 class TaskListController: UITableViewController {
+    var tasksStorage: TasksStorageProtocol = TasksStorage()
+    var tasks: [TaskPriority:[TaskProtocol]] = [:]
+    var sectionsTypesPosition: [TaskPriority] = [.important, .normal]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadTasks()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -12,27 +16,65 @@ class TaskListController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    private func loadTasks() {
+        sectionsTypesPosition.forEach { taskType in
+            tasks[taskType] = []
+        }
+        tasksStorage.loadTasks().forEach { task in
+            tasks[task.type]?.append(task)
+        }
+    }
+
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return tasks.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let taskType = sectionsTypesPosition[section]
+        guard let currentTasksType = tasks[taskType] else {
+            return 0
+        }
+        return currentTasksType.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        return getConfiguredTaskCell_constrains(for: indexPath)
+    }
 
-        // Configure the cell...
-
+    private func getConfiguredTaskCell_constrains(for indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellConstraints", for: indexPath)
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let currentTask = tasks[taskType]?[indexPath.row] else {
+            return cell
+        }
+        let symbolLabel = cell.viewWithTag(1) as? UILabel
+        let textLabel = cell.viewWithTag(2) as? UILabel
+        symbolLabel?.text = getSymbolForTask(with: currentTask.status)
+        textLabel?.text = currentTask.title
+        
+        if currentTask.status == .planned {
+            textLabel?.textColor = .black
+            symbolLabel?.textColor = .black
+        } else {
+            textLabel?.textColor = .lightGray
+            symbolLabel?.textColor = .lightGray
+        }
         return cell
     }
-    */
+
+    private func getSymbolForTask(with status: TaskStatus) -> String {
+        var resultSymbol: String
+        if status == .planned {
+            resultSymbol = "\u{25CB}"
+        } else if status == .completed {
+            resultSymbol = "\u{25C9}"
+        } else {
+            resultSymbol = ""
+        }
+        return resultSymbol
+    }
 
     /*
     // Override to support conditional editing of the table view.
